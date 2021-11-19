58d6bf4bb38b25c44d7190084532e56c22032be1
sneed <75911464+bannnedb@users.noreply.github.com>
2021-11-19 18:04:21 +1100

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -99,6 +99,132 @@ ideologies = [
         }
     },
     {
+        "name": "Centrism",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "FALGSC",
+        "stats": {
+            "econ": 100,
+            "dipl": 100,
+            "govt": 50,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Revolutionary Progressivism",
+        "stats": {
+            "econ": 70,
+            "dipl": 70,
+            "govt": 30,
+            "scty": 90
+        }
+    },
+    {
+        "name": "Bolshevism",
+        "stats": {
+            "econ": 90,
+            "dipl": 80,
+            "govt": 20,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Stalinism",
+        "stats": {
+            "econ": 90,
+            "dipl": 30,
+            "govt": 10,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Trotskyism",
+        "stats": {
+            "econ": 90,
+            "dipl": 90,
+            "govt": 20,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Hive-Mind Collectivism",
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Fordism",
+        "stats": {
+            "econ": 0,
+            "dipl": 50,
+            "govt": 0,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Juche",
+        "stats": {
+            "econ": 90,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 40
+        }
+    },
+    {
+        "name": "Libertarian Socialism",
+        "stats": {
+            "econ": 75,
+            "dipl": 75,
+            "govt": 75,
+            "scty": 75
+        }
+    },
+    {
+        "name": "Anarcho-Socialism",
+        "stats": {
+            "econ": 80,
+            "dipl": 80,
+            "govt": 90,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Conservative Libertarianism",
+        "stats": {
+            "econ": 20,
+            "dipl": 30,
+            "govt": 70,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Anarcho-Capitalism",
+        "stats": {
+            "econ": 0,
+            "dipl": 50,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Neoconservatism",
+        "stats": {
+            "econ": 40,
+            "dipl": 40,
+            "govt": 35,
+            "scty": 35
+        }
+    },
+    {
         "name": "Falangism",
         "stats": {
             "econ": 60,
