3fe6645ed9c019a842b0749335ff3cabd549f1c2
sneed <75911464+bannnedb@users.noreply.github.com>
2021-11-19 17:23:54 +1100

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,29 +1,110 @@
 ideologies = [
     {
-        "name": "Anarcho-Communism",
+        "name": "Reactionary Centrism",
         "stats": {
-            "econ": 100,
-            "dipl": 50,
-            "govt": 100,
-            "scty": 90
+            "econ": 50,
+            "dipl": 15,
+            "govt": 40,
+            "scty": 10
         }
     },
     {
-        "name": "Libertarian Communism",
+        "name": "Catholic Integralism",
         "stats": {
-            "econ": 100,
-            "dipl": 70,
-            "govt": 80,
-            "scty": 80
+            "econ": 60,
+            "dipl": 25,
+            "govt": 30,
+            "scty": 10
         }
     },
     {
-        "name": "Anarcho-Capitalism",
+        "name": "Paleoconservatism",
         "stats": {
-            "econ": 0,
+            "econ": 35,
+            "dipl": 15,
+            "govt": 30,
+            "scty": 20
+        }
+    },
+    {
+        "name": "National Syndicalism",
+        "stats": {
+            "econ": 75,
+            "dipl": 20,
+            "govt": 25,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Distributism",
+        "stats": {
+            "econ": 60,
             "dipl": 50,
-            "govt": 100,
+            "govt": 40,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Paternalistic Conservatism",
+        "stats": {
+            "econ": 65,
+            "dipl": 25,
+            "govt": 30,
+            "scty": 25
+        }
+    },
+    {
+        "name": "Right-Wing Populism",
+        "stats": {
+            "econ": 40,
+            "dipl": 25,
+            "govt": 40,
+            "scty": 25
+        }
+    },
+    {
+        "name": "Reactionary Liberalism",
+        "stats": {
+            "econ": 30,
+            "dipl": 15,
+            "govt": 60,
+            "scty": 15
+        }
+    },
+    {
+        "name": "Left-Wing Populism",
+        "stats": {
+            "econ": 70,
+            "dipl": 75,
+            "govt": 45,
+            "scty": 75
+        }
+    },
+    {
+        "name": "Classical Liberalism",
+        "stats": {
+            "econ": 20,
+            "dipl": 40,
+            "govt": 65,
             "scty": 50
+        }
+    },
+    {
+        "name": "English Socialism (IngSoc)",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Falangism",
+        "stats": {
+            "econ": 60,
+            "dipl": 10,
+            "govt": 20,
+            "scty": 10
         }
     }
 ];
